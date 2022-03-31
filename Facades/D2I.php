0a94d558b512b403a608bba5f03d2a835fee6778
Mr. VnS <mrvns@live.com>
2022-03-31 14:13:58 +0700

    Create D2I.php

---
+++ Facades/D2I.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace ToiLaDev\DocToImage\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class D2I extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'D2I';
+    }
+}
